0d7c6478b5a0611739271894b419d37eff3cf71b
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-10 11:23:05 +0000

    add path alias

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
-import { ToggleColorMode } from "./components/ToggleColorMode";
+import { ToggleColorMode } from "@/components/ToggleColorMode";
 
 function LightBulbIcon(props: SvgIconProps) {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import theme from "./theme";
-import App from "./App";
+import theme from "@/theme";
+import App from "@/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
