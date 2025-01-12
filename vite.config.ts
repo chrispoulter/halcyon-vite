803976fc8e75712213f6b852300d363a10baffe8
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-12 14:27:08 +0000

    use dynamic vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
-    server: {
-        watch: {
-            usePolling: process.env.USE_POLLING === 'true',
-        },
-        proxy: {
-            '/api': {
-                target: process.env.API_URL,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-                secure: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
             },
         },
-    },
+        server: {
+            watch: {
+                usePolling: env.USE_POLLING === 'true',
+            },
+            proxy: {
+                '/api': {
+                    target: env.API_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                    secure: false,
+                },
+            },
+        },
+    };
 });
