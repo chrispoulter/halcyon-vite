d57b7346963218d033e4d902d891cffc6743c08f
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-16 13:27:51 +0000

    mui v15

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,29 @@
-import { createTheme } from '@mui/material/styles';
+import { adaptV4Theme, createTheme } from '@mui/material/styles';
 
-export const theme = createTheme({
-    colorSchemes: {
-        dark: true,
-    },
-});
+export const theme = createTheme(
+    adaptV4Theme({
+        palette: {
+            mode: 'dark',
+        },
+        // overrides: {
+        //     MuiInputLabel: {
+        //         root: {
+        //             color: 'white',
+        //             backgroundColor: 'pink',
+        //         },
+        //     },
+        //     MuiInputBase: {
+        //         root: {
+        //             color: 'yellow',
+        //             backgroundColor: 'green',
+        //         },
+        //     },
+        //     MuiTextField: {
+        //         root: {
+        //             color: 'red',
+        //             backgroundColor: 'blue',
+        //         },
+        //     },
+        // },
+    })
+);
