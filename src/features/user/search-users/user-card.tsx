d2385f0fcab1b97f5bb77cb4ede2283122c59ac6
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-16 17:06:43 +0000

    add chips to cards

--- src/features/user/search-users/user-card.tsx
+++ src/features/user/search-users/user-card.tsx
@@ -1,6 +1,14 @@
 import { Link as RouterLink } from 'react-router';
-import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import {
+    Box,
+    Card,
+    CardActionArea,
+    CardContent,
+    Chip,
+    Typography,
+} from '@mui/material';
 import { SearchUsersResponse } from '@/features/user/user-types';
+import { roles } from '@/lib/session-types';
 
 type UserCardProps = {
     user: SearchUsersResponse['items'][number];
@@ -10,11 +18,39 @@ export function UserCard({ user }: UserCardProps) {
     return (
         <Card component={RouterLink} to={`/user/${user.id}`}>
             <CardActionArea>
-                <CardContent>
+                <CardContent
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 1,
+                    }}
+                >
                     <Typography variant="h6">
                         {user.firstName} {user.lastName}
                     </Typography>
                     <Typography variant="body2">{user.emailAddress}</Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: { xs: 'column', sm: 'row' },
+                            gap: 1,
+                        }}
+                    >
+                        {user.isLockedOut && (
+                            <Chip
+                                variant="outlined"
+                                color="error"
+                                label="Locked"
+                            />
+                        )}
+                        {user.roles?.map((role) => (
+                            <Chip
+                                key={role}
+                                variant="outlined"
+                                label={roles[role].title}
+                            />
+                        ))}
+                    </Box>
                 </CardContent>
             </CardActionArea>
         </Card>
