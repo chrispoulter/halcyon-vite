6c7498ab5709a298315c603f59c11bfa11b39450
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-23 13:51:30 +0000

    update config replacement

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo "Updating runtime environment variables..."
-
 find /usr/share/nginx/html/assets -type f -name "*.js" -print0 | xargs -0 sed -i \
-	-e "s|VITE_API_URL_PLACEHOLDER|${VITE_API_URL}|g"
+	-e "s|VITE_API_URL:void 0|VITE_API_URL: \"${VITE_API_URL}\"|g"
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
     VERSION: import.meta.env.npm_package_version,
-    VITE_API_URL: import.meta.env.VITE_API_URL || 'VITE_API_URL_PLACEHOLDER',
+    VITE_API_URL: import.meta.env.VITE_API_URL,
 };
